3e8ccc21f0686bef4941fc43ed8ce326a03bf8db
JackerZet <jackerzet@gmail.com>
2023-02-13 18:49:42 +0300

    1 lesson

---
+++ Assets/_Root/Scripts/Game/InputLogic/InputKeyboardView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Game.InputLogic
+{
+    internal class InputKeyboardView : BaseInputView
+    {
+        [SerializeField] private float _inputMultiplier = 1f;
+
+        protected override void Move()
+        {
+            float axisOffset = Input.GetAxis("Horizontal");
+            float moveValue = _speed * _inputMultiplier * Time.deltaTime * axisOffset;
+
+            float abs = Mathf.Abs(moveValue);
+            float sign = Mathf.Sign(moveValue);
+
+            if (sign > 0)
+                OnRightMove(abs);
+            else if (sign < 0)
+                OnLeftMove(abs);
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/Ui/Settings/SettingsMenuView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Ui.Settings
+{
+    public class SettingsMenuView : MonoBehaviour
+    {
+        [SerializeField] private Button _buttonBack;
+        
+        public void Init(UnityAction backGame)
+        {
+            _buttonBack.onClick.AddListener(backGame);           
+        }
+
+        public void OnDestroy()
+        {
+            _buttonBack.onClick.RemoveAllListeners();           
+        }
+    }
+}
